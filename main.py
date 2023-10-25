0f72f7c9ebc791a8b75e9a6b25734707ca6baeb5
Nastie <nstalennaya@gmail.com>
2023-10-25 19:51:30 +0300

    dfghjkl

---
+++ main.py
@@ -0,0 +1,5 @@
+country = 'Україна'
+association = 'насмачнішим борщем'
+smile = '\U0001FAD5'
+sum = country + ', вона в мене асоціюється з ' + association
+print(f'Моя улюблена країна - це {sum} {smile}')
