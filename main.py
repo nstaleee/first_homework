544e85da8e77d1104eb41aa76091d3649e5b36f1
nstaleee <139904009+nstaleee@users.noreply.github.com>
2023-10-25 19:54:47 +0300

    Revert "dfghjkl"

--- main.py
+++
@@ -1,5 +0,0 @@
-country = 'Україна'
-association = 'насмачнішим борщем'
-smile = '\U0001FAD5'
-sum = country + ', вона в мене асоціюється з ' + association
-print(f'Моя улюблена країна - це {sum} {smile}')
